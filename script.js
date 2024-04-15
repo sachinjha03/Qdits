5388171ea44550d84192c217fd1c46e942045dfc
Sachin Jha <139990682+sachinjha03@users.noreply.github.com>
2024-04-15 17:11:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.querySelector(".landing-section-right");
+    const images = document.querySelectorAll(".random-image");
+  
+    images.forEach(function(image) {
+      const randomX = Math.random() * (container.offsetWidth - image.offsetWidth);
+      const randomY = Math.random() * (container.offsetHeight - image.offsetHeight);
+      
+      image.style.left = randomX + "px";
+      image.style.top = randomY + "px";
+    });
+
+
+  });
+
